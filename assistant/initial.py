ca8b372b1f6686c7a61fdb1c80faf5989068c654
Keysmith <113760925+KojiraReyyAnata@users.noreply.github.com>
2023-12-27 02:51:36 +0700

    update initial.py

--- assistant/initial.py
+++ assistant/initial.py
@@ -31,7 +31,7 @@ Beberapa Bantuan Untuk Kamu Pelajari.""",
 -> Kamu dapat berinteraksi dengan seseorang melali fitur chatbot
 
 **• To Know About Updates**
-  - Join @kynansupport.""",
+  - Join @skyfand.""",
     4: f"""**• Bantuan yang mungkin kamu ingin lihat •**
 
   - `{HNDLR}help`
